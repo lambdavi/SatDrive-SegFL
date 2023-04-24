692dda53f599365a5c75c481e6d3bdf2b51f6eb0
Davide Buoso <daviibuoso@gmail.com>
2023-04-24 17:11:06 +0200

    Client test function implemented 2

--- datasets/idda.py
+++ datasets/idda.py
@@ -17,7 +17,7 @@ class IDDADataset(VisionDataset):
 
     def __init__(self,
                  root: str,
-                 list_samples: list(str),
+                 list_samples: list[str],
                  transform: tr.Compose = None,
                  client_name: str = None):
         super().__init__(root=root, transform=transform, target_transform=None)
