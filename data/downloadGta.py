d5a1211ba2500d0d34b20543fbb9df44a945dd49
Davide Buoso <daviibuoso@gmail.com>
2023-05-05 18:20:37 +0200

    added download gta script

---
+++ data/downloadGta.py
@@ -0,0 +1,12 @@
+#!/bin/python
+from pymegatools import Megatools
+import subprocess
+import os
+mega = Megatools()
+print("Version:", mega.version)
+url = "https://mega.nz/file/ERkiQBaY#h-wktK7U7MpIG5nf-rMWF7d76NEM5ae_MrAmELftNR0"
+# Get a file name from url
+print(mega.filename(url))
+
+# Downloading a file from url
+mega.download(url)
--- data/downloadGta.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-LINK_TO_GTA5="https://mega.nz/file/ERkiQBaY#h-wktK7U7MpIG5nf-rMWF7d76NEM5ae_MrAmELftNR0"
-sudo apt-get update
-sudo apt-get install megatools unzip -y
-megadl $LINK_TO_GTA5
-unzip -q *.zip
-rm *.zip
