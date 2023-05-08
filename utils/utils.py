7c8dd4ebbae332533a588fde1c11fff6fc049dde
Davide Buoso <daviibuoso@gmail.com>
2023-05-08 10:35:14 +0200

    added balanced clients

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import numpy as np
 
 # Internal function
-def split_list_numpy(lst, m):
+def split_list_random(lst, m):
     n = len(lst)
     if m <= 0 or n <= 0:
         return []
@@ -14,6 +14,24 @@ def split_list_numpy(lst, m):
         sizes = np.concatenate(([sizes[0]], sizes[1:] - sizes[:-1], [n - sizes[-1]]))
         return [lst[sum(sizes[:i]):sum(sizes[:i+1])] for i in range(m)]
 
+def split_list_balanced(lst, m):
+    n = len(lst)
+    if m <= 0 or n <= 0:
+        return []
+    elif m >= n:
+        return [lst[i:i+1] for i in range(n)]
+    else:
+        # Compute the size of each sublist
+        sublist_size = n // m
+        sizes = np.full(m, sublist_size, dtype=np.int32)
+        remainder = n % m
+        if remainder != 0:
+            # Add the remaining elements to the first few sublists
+            sizes[:remainder] += 1
+
+        # Create the sublists by slicing the input list
+        sublists = [lst[sum(sizes[:i]):sum(sizes[:i+1])] for i in range(m)]
+        return sublists
 
 class HardNegativeMining(nn.Module):
 
