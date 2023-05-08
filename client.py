3402e51dd226308e1a16dfb7348c2498a4d4c63d
Davide Buoso <daviibuoso@gmail.com>
2023-05-08 10:18:08 +0200

    fix

--- client.py
+++ client.py
@@ -46,7 +46,6 @@ class Client:
         :param cur_epoch: current epoch of training
         :param optimizer: optimizer used for the local training
         """
-        print(len(self.train_loader))
         for cur_step, (images, labels) in enumerate(self.train_loader):
             images = images.to(self.device, dtype=torch.float32)
             labels = labels.to(self.device, dtype=torch.long)
