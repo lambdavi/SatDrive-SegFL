69bef9a97c9780239b6d511e47e60f7d5e63dd6a
Davide Buoso <daviibuoso@gmail.com>
2023-05-19 14:29:23 +0200

    modified Fdaserver

--- fda_server.py
+++ fda_server.py
@@ -34,7 +34,7 @@ class FdaServer:
         print(f"N-clients: {len(self.train_clients)}")
         for c in self.train_clients:
             print(type(c))
-            self.styleaug.add_style(c)
+            self.styleaug.add_style(c.train_loader)
         
     def train_round(self, clients):
         """
