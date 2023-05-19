bd650c385cd6dabda3af0774f4c0da9eee0dd11e
Davide Buoso <daviibuoso@gmail.com>
2023-05-19 10:20:00 +0200

    early modification for fda setting

--- utils/args.py
+++ utils/args.py
@@ -1,12 +1,16 @@
 import argparse
 
+def str2tuple(tp=int):
+
+    def convert(s):
+        return tuple(tp(i) for i in s.split(','))
+    return convert
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, default=0, help='random seed')
     parser.add_argument('--dataset', type=str, choices=['idda', 'femnist', 'gta5'], required=True, help='dataset name')
-    parser.add_argument('--niid', action='store_true', default=False,
-                        help='Run the experiment with the non-IID partition (IID by default). Only on FEMNIST dataset.')
+    parser.add_argument('--niid', action='store_true', default=False,  help='Run the experiment with the non-IID partition (IID by default). Only on FEMNIST dataset.')
     parser.add_argument('--model', type=str, choices=['deeplabv3_mobilenetv2', 'resnet18', 'cnn'], help='model name')
     parser.add_argument('--num_rounds', type=int, help='number of rounds')
     parser.add_argument('--num_epochs', type=int, help='number of local epochs')
@@ -24,5 +28,8 @@ def get_parser():
     parser.add_argument('--centr', action='store_true', default=False, help='Only one client will be used if set True')
     parser.add_argument('--opt', type=str, choices=['SGD', 'adam'], default = 'SGD', help='Optimizer choice')
     parser.add_argument('--sched', type=str, choices=['lin', 'step'], default = None, help='Scheduler choice')
-
+    parser.add_argument('--n_images_per_style', type=int, default=100, help='number of images to extract style (avg is performed)')
+    parser.add_argument('--fda_L', type=float, default=0.01, help='to control size of amplitude window')
+    parser.add_argument('--fda_b', type=int, default=None, help='if != None it is used instead of fda_L:' 'b == 0 --> 1x1, b == 1 --> 3x3, b == 2 --> 5x5, ...')
+    parser.add_argument('--fda_size', type=str2tuple(int), default='1024,512', help='size (W,H) to which resize images before style transfer')
     return parser
