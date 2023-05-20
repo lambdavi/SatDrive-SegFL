95d3f1f2f22efcc5f086a30069de4de9cb61a594
Marco Castiglia <cstmrc01d13e532x@studium.unict.it>
2023-05-20 19:07:00 +0200

    Fixed EarlyStopper

--- utils/early_stopping.py
+++ utils/early_stopping.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 class EarlyStopper:
-    def __init__(self, patience=1, min_delta=0):
+    def __init__(self, patience=3, min_delta=0.05):
         self.patience = patience
         self.min_delta = min_delta
         self.counter = 0
         self.min_validation_loss = np.inf
 
     def early_stop(self, validation_loss):
-        if validation_loss < self.min_validation_loss:
-            self.min_validation_loss = validation_loss
+        if (validation_loss - self.min_validation_loss) > self.min_delta:
             self.counter = 0
-        elif validation_loss > (self.min_validation_loss + self.min_delta):
+        elif abs(validation_loss - self.min_validation_loss) < self.min_delta:
             self.counter += 1
             if self.counter >= self.patience:
                 return True
+        self.min_validation_loss = validation_loss
         return False
