559b35096d93584525de771b5d11d36cdc7cf0f3
Davide Buoso <daviibuoso@gmail.com>
2023-05-20 18:41:36 +0200

    inserted prediction mode

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ from collections import OrderedDict
 
 import numpy as np
 import torch
-
+from PIL import Image
+import datasets.ss_transforms as sstr
 
 class Server:
     def __init__(self, args, train_clients, test_clients, model, metrics, valid=False, valid_clients=None):
@@ -74,28 +75,35 @@ class Server:
         if self.args.centr:
             num_rounds = 1
         
-        for r in range(num_rounds):
-            print("------------------")
-            print(f"Round {r+1}/{num_rounds} started.")
-            print("------------------")
+        if self.args.load:
+            self.model.load_state_dict(torch.load('path_to_saved_model.pth'))
+        else:
+            for r in range(num_rounds):
+                print("------------------")
+                print(f"Round {r+1}/{num_rounds} started.")
+                print("------------------")
 
-            # Select random subset of clients
-            chosen_client = self.select_clients(seed=r)
-            # Train a round
-            updates = self.train_round(chosen_client)
-            # Aggregate the parameters
-            self.model_params_dict = self.aggregate(updates)
-            self.model.load_state_dict(self.model_params_dict, strict=False)
-            if self.activate_val:
-                self.eval_validation()
+                # Select random subset of clients
+                chosen_client = self.select_clients(seed=r)
+                # Train a round
+                updates = self.train_round(chosen_client)
+                # Aggregate the parameters
+                self.model_params_dict = self.aggregate(updates)
+                self.model.load_state_dict(self.model_params_dict, strict=False)
+                if self.activate_val:
+                    self.eval_validation()
 
-        
-        if self.args.dataset != "gta5":  
-            print("------------------------------------")
-            print(f"Evaluation of the trainset started.")
-            print("------------------------------------")      
-            self.eval_train()
-        self.test()
+            
+            if self.args.dataset != "gta5":  
+                print("------------------------------------")
+                print(f"Evaluation of the trainset started.")
+                print("------------------------------------")      
+                self.eval_train()
+            self.test()
+
+            if self.args.save:
+                torch.save(self.model.state_dict(), 'path_to_save_model.pth')
+
 
     def eval_train(self):
         """
@@ -137,4 +145,36 @@ class Server:
         res=self.metrics["test_diff_dom"].get_results()
         print(f'Acc: {res["Overall Acc"]}, Mean IoU: {res["Mean IoU"]}')
 
-        
+    def predict(self, image_path):
+        # Load and preprocess the input image
+        input_image = Image.open(image_path)
+
+        # Apply necessary transformations
+        transforms= sstr.Compose([
+            sstr.ToTensor(),
+            sstr.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        input_tensor = transforms(input_image).unsqueeze(0)  # Add batch dimension
+
+        self.model.eval()
+
+        # Perform inference
+        with torch.no_grad():
+            output = self.model(input_tensor)['out']  # Get the output logits
+
+        # Convert logits to class probabilities
+        probs = torch.softmax(output, dim=1)[0]
+
+        # Retrieve predicted class labels
+        _, class_labels = torch.max(probs, dim=0)
+
+        # Optional: Convert class labels to a colored segmentation mask
+        color_map = self.model.classifier[-1].weight.squeeze().numpy()
+        segmentation_mask = color_map[class_labels.cpu().numpy()]
+
+        # Save the segmentation mask as an image
+        output_image = Image.fromarray(segmentation_mask.astype('uint8'))
+        output_image.save('saved_ouput.png')
+
+
