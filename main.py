90c53979e0df58b10da636edb4c1279ac97fbc78
Davide Buoso <daviibuoso@gmail.com>
2023-05-21 18:49:18 +0200

    added arg for es

--- main.py
+++ main.py
@@ -274,7 +274,7 @@ def main():
 
     server.train()
 
-    server.predict("data/gta5_testimage.jpg")
+    server.predict("data/idda/images/28560_T03_CS_J.jpg")
 
 if __name__ == '__main__':
     main()
