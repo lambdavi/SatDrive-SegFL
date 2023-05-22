3eba63aa6a5135e6664b8c8ed7c7f7e6d200eb3a
Davide Buoso <daviibuoso@gmail.com>
2023-05-22 12:26:33 +0200

    made changes

--- fda_server.py
+++ fda_server.py
@@ -21,8 +21,8 @@ class FdaServer:
 
         # Style transfer
         self.styleaug = StyleAugment(args.n_images_per_style, args.fda_L, args.fda_size, b=args.fda_b) 
-        if not self.args.load:
-            self.extract_styles()
+        
+        self.extract_styles()
         
     def select_clients(self, seed=None):
         num_clients = min(self.args.clients_per_round, len(self.train_clients))
