be29469ae350624bd071b0ad042f0f3a4bde8279
Davide Buoso <daviibuoso@gmail.com>
2023-05-22 21:16:34 +0200

    made changes

--- main.py
+++ main.py
@@ -268,7 +268,7 @@ def main():
         else: 
             server = Server(args, train_clients, test_clients, model, metrics)
     else:
-        print("Fda Mode activated.")
+        print("FDA mode activated.")
         server = FdaServer(args, source_dataset, train_clients, test_clients, model, metrics)
 
     #server.train()
