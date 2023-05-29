52ba6c2e337c2f172622902a63f226ad39460f8a
Davide Buoso <daviibuoso@gmail.com>
2023-05-29 11:17:21 +0200

    fix

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def get_transforms(args):
                 sstr.RandomResizedCrop((512, 512), scale=(0.5, 2.0)),
                 sstr.ToTensor(),
                 sstr.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-            ]),
+            ])
         test_transforms = sstr.Compose([
             sstr.ToTensor(),
             sstr.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
