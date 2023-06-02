1c948904bd797bc368346f054e2b6d82f6a68c3b
Davide Buoso <daviibuoso@gmail.com>
2023-06-02 20:17:26 +0200

    fixes in args

--- utils/args.py
+++ utils/args.py
@@ -17,7 +17,7 @@ def get_parser():
     parser.add_argument('--transformer_model', type=str, choices=['b0', 'b1', 'b2'], default="b0", help='weights of the transformers b0 lighter - b2 heavier')
     parser.add_argument('--num_rounds', type=int, default=1, help='number of rounds')
     parser.add_argument('--num_epochs', type=int, default=1, help='number of local epochs')
-    parser.add_argument('--clients_per_round', type=int, help='number of clients trained per round')
+    parser.add_argument('--clients_per_round', type=int, default=1, help='number of clients trained per round')
     parser.add_argument('--hnm', action='store_true', default=False, help='Use hard negative mining reduction or not')
     parser.add_argument('--lr', type=float, default=0.05, help='learning rate')
     parser.add_argument('--bs', type=int, default=4, help='batch size')
