5cd575b4a3226fdf3cea602ce747d995b6805c1f
Davide Buoso <daviibuoso@gmail.com>
2023-06-09 18:31:44 +0200

    fix

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def model_init(args):
 
 def get_transforms(args):
     if args.model in ["segformer",'deeplabv3_mobilenetv2']:
-        if args.dataset == "loveda" or args.model == "segformer":
+        if args.dataset == "loveda":
             train_transforms = sstr.Compose([
                 sstr.RandomCrop((512, 512)),
                 sstr.RandomHorizontalFlip(),
@@ -87,7 +87,7 @@ def get_transforms(args):
                     RandomApply([sstr.Lambda(lambda x: weather.add_rain(x))], p=0.15),
                 ]),
                 sstr.Compose([
-                    sstr.RandomCrop((512, 928)),
+                    sstr.RandomCrop((512, 928 if args.model != "segformer" else 512)),
                     sstr.ToTensor(),
                     sstr.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                 ]), 
