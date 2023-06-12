70389ead68668ece1c9094901d11ac3e46d65eeb
Davide Buoso <daviibuoso@gmail.com>
2023-06-12 14:30:34 +0200

    added hroiz

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def get_transforms(args):
         if args.dataset == "loveda":
             train_transforms = sstr.Compose([
                 sstr.RandomCrop((512, 512)),
-                sstr.RandomVerticalFlip(),
+                sstr.RandomHorizontalFlip(),
                 sstr.ToTensor(),
                 sstr.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
             
