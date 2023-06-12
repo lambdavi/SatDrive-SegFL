e625f3d307aabbaa0ec1610e90c17189629b2d6e
Davide Buoso <daviibuoso@gmail.com>
2023-06-12 21:55:23 +0200

    fixed loveda download file

--- downloadLoveda.py
+++ downloadLoveda.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 mega = Megatools()
 print("Version:", mega.version)
-url = "https://mega.nz/file/dONCFICZ#Wtbbyxr9N0Yeqf6sTP9mPKH_aDwuRZ0IbUxWPI_zyYY"
+url = "https://mega.nz/file/QSUUDSyA#uYk3VksViA9vb0dQhSSA0fYAMdG6PL84mT5ShOnfipQ"
 # Get a file name from url
 print(mega.filename(url))
 
