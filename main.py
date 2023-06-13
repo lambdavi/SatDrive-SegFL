e3cd4e52a4b1f591b17e3305b3d151425bfc4384
Davide Buoso <daviibuoso@gmail.com>
2023-06-13 19:12:17 +0200

    hotiz

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def get_transforms(args):
                     sstr.RandomHorizontalFlip(),
                     sstr.ToTensor(),
                     sstr.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-                ]), 
+                ])
             
         test_transforms = sstr.Compose([
             sstr.ToTensor(),
