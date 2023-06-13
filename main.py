85d2518d17a500b84a648b5546e2f5d983cb09c9
Davide Buoso <daviibuoso@gmail.com>
2023-06-13 21:28:51 +0200

    rotation

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def get_transforms(args):
         if args.dataset == "loveda":
             train_transforms = sstr.Compose([
                 sstr.RandomCrop((512, 512)),
-                sstr.RandomVerticalFlip(),
+                sstr.RandomRotation(45),
                 sstr.ToTensor(),
                 sstr.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
             
