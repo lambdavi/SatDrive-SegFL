b4f59887e75cdc788d3bf9f42c91e96c86d8f70e
Davide Buoso <daviibuoso@gmail.com>
2023-06-14 23:12:31 +0200

    fix

--- client.py
+++ client.py
@@ -150,7 +150,7 @@ class Client:
             optimizer.zero_grad()
             images = images.to(self.device, dtype=torch.float32)
             outputs = self._get_outputs(images, _)
-            c = crit(outputs, images, seg=seg)
+            c = crit(outputs, images)
             p = pseudo(outputs)
             loss = red(c, p)
             optimizer.step()
