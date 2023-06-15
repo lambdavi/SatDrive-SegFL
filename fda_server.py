fa7ca68d0dc8454034a794f106a1c1732080cc4c
Davide Buoso <daviibuoso@gmail.com>
2023-06-15 11:57:34 +0200

    test clients

--- fda_server.py
+++ fda_server.py
@@ -231,7 +231,7 @@ class FdaServer:
                 print("------------------")
 
                 # Select random subset of clients
-                chosen_clients = self.select_clients(seed=r)
+                chosen_clients = self.select_clients()
                 
                 # Train a round
                 updates = self.train_round(chosen_clients)
